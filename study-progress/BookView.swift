068bacf52e52a89f7957bf0d0ad4f672b139342c
Blue_S <aoicchi0620@gmail.com>
2022-09-18 19:08:21 +0900

    [add] make NavigationView from BookView to SectionView

--- study-progress/BookView.swift
+++ study-progress/BookView.swift
@@ -4,37 +4,55 @@
 
 import SwiftUI
 
-struct Book: Hashable {
+struct Book {
     let title: String
     let author: String
     var sectionNum: Int = 10
-    var comment: String = ""
+    var sectionDetail: [(Int, String)] = Array(repeating: (2, "good"), count: 10)
+}
+extension Book: Hashable {
+    static func ==(lhs: Book, rhs: Book) -> Bool {
+        lhs.title == rhs.title && lhs.author == rhs.author
+    }
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(title)
+        hasher.combine(author)
+    }
 }
 
 struct BookView: View {
     var book = Book(title: "no book", author: "no author")
     var body: some View {
         VStack {
-            Text("\(book.title)")
-            Text("\(book.author)")
-            ForEach(1 ... book.sectionNum, id: \.self) { section in
-                SectionView(num: section)
+            NavigationView {
+                List {
+                    ForEach(1 ... book.sectionNum, id: \.self) { section in
+                        NavigationLink(destination: SectionView(book: book, section: section)) {
+                            HStack {
+                                Spacer()
+                                Text("section \(section)")
+                                Spacer()
+                                ForEach(1...2, id: \.self) { _ in
+                                    Image(systemName: "star.fill")
+                                }
+                                ForEach(2..<5, id: \.self) { _ in
+                                    Image(systemName: "star")
+                                }
+                                Spacer()
+                            }
+                        }
+                                .navigationBarTitle(book.title)
+                    }
+                }
             }
         }
     }
 }
 
-struct SectionView: View {
-    var num: Int = 1
+struct SectionView : View {
+    var book = Book(title: "no book", author: "no author")
+    var section = 1
     var body: some View {
-        HStack {
-            Text("section \(num)")
-            ForEach(1 ... 2, id: \.self) { _ in
-                Image(systemName: "star.fill")
-            }
-            ForEach(2 ..< 5, id: \.self) { _ in
-                Image(systemName: "star")
-            }
-        }
+        Text(book.sectionDetail[section].1)
     }
 }
